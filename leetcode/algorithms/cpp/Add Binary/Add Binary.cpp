92897ec03a854624817c3a07b2941dd5c3352abd
DuckTyping <1394466835@qq.com>
2023-07-03 21:43:38 +0800

    feat: update

--- leetcode/algorithms/cpp/Add Binary/Add Binary.cpp
+++ leetcode/algorithms/cpp/Add Binary/Add Binary.cpp
@@ -1,8 +1,30 @@
+#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-  string addBinary(string a, string b) {}
+  string addBinary(string a, string b) {
+    string res{};
+    int i = a.size() - 1, j = b.size() - 1;
+    int sum = 0, carry = 0;
+    while (i >= 0 || j >= 0) {
+      if (i >= 0) {
+        sum += (a[i] - '0');
+        i--;
+      }
+      if (j >= 0) {
+        sum += (b[j] - '0');
+        j--;
+      }
+      res.push_back((sum % 2) + '0');
+      sum /= 2;
+    }
+    if (sum > 0) {
+      res.push_back(sum + '0');
+    }
+    std::reverse(res.begin(), res.end());
+    return res;
+  }
 };
