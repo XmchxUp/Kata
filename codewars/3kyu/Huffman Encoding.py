3e6ff6862e860b9dd686b0ab11dd634d43abf77b
DuckTyping <1394466835@qq.com>
2023-07-11 22:23:16 +0800

    update

---
+++ codewars/3kyu/Huffman Encoding.py
@@ -0,0 +1,13 @@
+# takes: str; returns: [ (str, int) ] (Strings in return value are single characters)
+def frequencies(s):
+    return []
+
+
+# takes: [ (str, int) ], str; returns: String (with "0" and "1")
+def encode(freqs, s):
+    return ""
+
+
+# takes [ [str, int] ], str (with "0" and "1"); returns: str
+def decode(freqs, bits):
+    return ""
