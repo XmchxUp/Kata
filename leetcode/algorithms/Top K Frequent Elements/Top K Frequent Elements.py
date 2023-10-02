529683746bbdc8150fbc9ebf97a4953521d7124c
ChillyForest <1394466835@qq.com>
2023-10-02 08:29:11 +0800

    chore: update

--- leetcode/algorithms/Top K Frequent Elements/Top K Frequent Elements.py
+++ leetcode/algorithms/Top K Frequent Elements/Top K Frequent Elements.py
@@ -2,6 +2,38 @@ from heapq import heappush, heappop
 from collections import Counter
 
 
+# 桶排序
+# Time Complexity O(n)
+# Space Complexity O(n)
+class Solution2(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        m = dict()
+        for num in nums:
+            if num not in m:
+                m[num] = 0
+            m[num] += 1
+        tmpArr = [[] for _ in range((len(nums) + 1))]
+        for num, freq in m.items():
+            tmpArr[freq].append(num)
+        res = []
+        size = k
+        for i in range(len(tmpArr) - 1, 0, -1):
+            if k == 0:
+                break
+            if tmpArr[i]:
+                res += tmpArr[i]
+                k -= 1
+        return res[:size]
+
+
+# 最小堆
+# Time Complexity O(NlogK)
+# Space Complexity O(N)
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
