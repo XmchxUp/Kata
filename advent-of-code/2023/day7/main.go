0d4cf55ac6bdf84d5523c5f8a524fe3ed7a658dd
ultraman <1394466835@qq.com>
2024-07-27 10:13:10 +0800

    update

--- advent-of-code/2023/day7/main.go
+++ advent-of-code/2023/day7/main.go
@@ -32,7 +32,7 @@ var cardValues = map[Card]int{
 	"8": 8,
 	"9": 9,
 	"T": 10,
-	"J": 11,
+	"J": 1, // phase2 is weakest
 	"Q": 12,
 	"K": 13,
 	"A": 14, // Ace is high
@@ -50,6 +50,54 @@ func NewHand(cards []Card, bid int) *Hand {
 	return h
 }
 
+// phase2
+func (h *Hand) updateType2(m map[string]int, pairCnt int, hasThree bool) {
+	if h.CCT == FiveOfAKind {
+		return
+	}
+
+	v, ok := m["J"]
+	if !ok {
+		return
+	}
+
+	if h.CCT == FourOfAKind {
+		h.CCT = FiveOfAKind
+		return
+	}
+
+	switch v {
+	case 1:
+		if hasThree {
+			h.CCT = FourOfAKind
+		} else if pairCnt == 0 {
+			h.CCT = OnePair
+		} else if pairCnt == 1 {
+			h.CCT = ThreeOfAKind
+		} else if pairCnt == 2 {
+			h.CCT = FullHouse
+		}
+	case 2:
+		if hasThree {
+			h.CCT = FiveOfAKind
+		} else if pairCnt == 0 {
+			h.CCT = ThreeOfAKind
+		} else if pairCnt == 1 {
+			h.CCT = ThreeOfAKind
+		} else if pairCnt == 2 {
+			h.CCT = FourOfAKind
+		}
+	case 3:
+		if pairCnt == 0 {
+			h.CCT = FourOfAKind
+		} else if pairCnt == 1 {
+			h.CCT = FiveOfAKind
+		}
+	case 4:
+		h.CCT = FiveOfAKind
+	}
+}
+
 func (h *Hand) updateType() {
 	m := make(map[string]int, 0)
 
@@ -57,16 +105,21 @@ func (h *Hand) updateType() {
 		m[string(c)] += 1
 	}
 
+	pairCnt := 0
+	hasThree := false
+
 	if len(m) == 1 {
 		h.CCT = FiveOfAKind
 	} else if len(m) == 5 {
 		h.CCT = HighCard
 	} else if len(m) == 4 {
 		h.CCT = OnePair
+		pairCnt = 1
 	} else {
-		pairCnt := 0
-		hasThree := false
-
+		// 22221
+		// 22233
+		// 33221
+		// 33312
 		for _, v := range m {
 			if v == 3 {
 				hasThree = true
@@ -78,14 +131,20 @@ func (h *Hand) updateType() {
 			}
 		}
 
-		if hasThree && pairCnt == 1 {
-			h.CCT = FullHouse
-		} else if pairCnt == 2 {
-			h.CCT = TwoPair
-		} else if hasThree {
-			h.CCT = ThreeOfAKind
+		if h.CCT != FourOfAKind {
+			if hasThree {
+				if pairCnt == 1 {
+					h.CCT = FullHouse
+				} else {
+					h.CCT = ThreeOfAKind
+				}
+			} else if pairCnt == 2 {
+				h.CCT = TwoPair
+			}
 		}
 	}
+
+	h.updateType2(m, pairCnt, hasThree)
 }
 
 func (h *Hand) Compare(another *Hand) bool {
@@ -123,16 +182,16 @@ func parseInput(input string) {
 	hands = append(hands, NewHand(cards, bid))
 }
 
-func getAnswer() int {
-	res := 0
+func getAnswer() uint32 {
+	var res uint32
 
 	sort.Slice(hands, func(i, j int) bool {
 		return hands[i].Compare(hands[j])
 	})
 
 	for i, h := range hands {
-		fmt.Println(i, h.Bid)
-		res += (i + 1) * h.Bid
+		// fmt.Println(i+1, h.Bid, h.Cards, h.CCT)
+		res += uint32(i+1) * uint32(h.Bid)
 	}
 
 	return res
