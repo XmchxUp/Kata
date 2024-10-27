6ca01e42f59811594e0a5015f57ca8423f84391a
ultraman <1394466835@qq.com>
2024-10-27 10:37:50 +0800

    update

--- advent-of-code/aoc2023/src/day13/mod.rs
+++ advent-of-code/aoc2023/src/day13/mod.rs
@@ -37,85 +37,146 @@ impl Runner for Aoc2023_13 {
     fn part1(&mut self) -> Vec<String> {
         vec![format!(
             "{}",
-            self.patterns.iter().map(|p| p.score).sum::<usize>()
+            self.patterns.iter().map(|p| p.score1).sum::<usize>()
         )]
     }
 
     fn part2(&mut self) -> Vec<String> {
-        todo!()
+        vec![format!(
+            "{}",
+            self.patterns.iter().map(|p| p.score2).sum::<usize>()
+        )]
     }
 }
 
 #[derive(Debug)]
 struct Pattern {
-    pub score: usize,
+    pub score1: usize,
+    pub score2: usize,
+}
+impl Pattern {
+    fn find_mirror(grid: &[String], allow_one_difference: bool) -> usize {
+        for x in 1..grid.len() {
+            let above: Vec<&String> = grid[..x].iter().rev().collect();
+            let below: Vec<&String> = grid[x..].iter().collect();
+
+            let min_len = std::cmp::min(above.len(), below.len());
+            let above = &above[..min_len];
+            let below = &below[..min_len];
+
+            if !allow_one_difference {
+                if above == below {
+                    return x;
+                }
+            } else {
+                let diff_count: usize = above
+                    .iter()
+                    .zip(below.iter())
+                    .map(|(a_row, b_row)| {
+                        a_row
+                            .chars()
+                            .zip(b_row.chars())
+                            .filter(|(a, b)| a != b)
+                            .count()
+                    })
+                    .sum();
+                if diff_count == 1 {
+                    return x;
+                }
+            }
+        }
+        0
+    }
+
+    fn transpose(grid: &[String]) -> Vec<String> {
+        let cols = grid[0].len();
+        let rows = grid.len();
+        let mut res: Vec<String> = vec![String::with_capacity(rows); cols];
+
+        for row in grid {
+            for (i, ch) in row.chars().enumerate() {
+                res[i].push(ch);
+            }
+        }
+
+        res
+    }
 }
 
 impl From<&Vec<String>> for Pattern {
-    fn from(value: &Vec<String>) -> Self {
-        let mut score = 0;
-        let rows = value.len();
-        let cols = value.get(0).unwrap().len();
+    fn from(grid: &Vec<String>) -> Self {
+        let mut score1 = 0;
+        let row = Self::find_mirror(grid, false);
+        score1 += row * 100;
+        let cols = Self::transpose(grid);
+        let col = Self::find_mirror(&cols, false);
+        score1 += col;
 
-        //  check horizontal
-        for r in 1..rows {
-            if value[r] != value[r - 1] {
-                continue;
-            }
+        let mut score2 = 0;
+        let row = Self::find_mirror(grid, true);
+        score2 += row * 100;
+        let col = Self::find_mirror(&cols, true);
+        score2 += col;
 
-            let mut i = (r - 1) as i32;
-            let mut j = r;
-            let mut found = true;
-            while i >= 0 && j < rows {
-                if value[i as usize] != value[j] {
-                    found = false;
-                    break;
-                }
-                i -= 1;
-                j += 1;
-            }
+        // part1
+        // //  check horizontal
+        // for r in 1..rows {
+        //     if grid[r] != grid[r - 1] {
+        //         continue;
+        //     }
 
-            if found {
-                score += 100 * r;
-                break;
-            }
-        }
+        //     let mut i = (r - 1) as i32;
+        //     let mut j = r;
+        //     let mut found = true;
+        //     while i >= 0 && j < rows {
+        //         if grid[i as usize] != grid[j] {
+        //             found = false;
+        //             break;
+        //         }
+        //         i -= 1;
+        //         j += 1;
+        //     }
 
-        // check verctial
-        let columns: Vec<Vec<char>> = (0..cols)
-            .into_iter()
-            .map(|c| {
-                value
-                    .iter()
-                    .map(|row| row.chars().nth(c).unwrap())
-                    .collect()
-            })
-            .collect();
+        //     if found {
+        //         score += 100 * r;
+        //         break;
+        //     }
+        // }
 
-        for c in 1..cols {
-            if columns[c] != columns[c - 1] {
-                continue;
-            }
+        // // check verctial
+        // let columns: Vec<Vec<char>> = (0..cols)
+        //     .map(|c| {
+        //         grid
+        //             .iter()
+        //             .map(|row| row.chars().nth(c).unwrap())
+        //             .collect()
+        //     })
+        //     .collect();
 
-            let mut i = (c - 1) as i32;
-            let mut j = c;
-            let mut found = true;
-            while i >= 0 && j < cols {
-                if columns[i as usize] != columns[j] {
-                    found = false;
-                    break;
-                }
-                i -= 1;
-                j += 1;
-            }
+        // for c in 1..cols {
+        //     if columns[c] != columns[c - 1] {
+        //         continue;
+        //     }
 
-            if found {
-                score += c;
-                break;
-            }
-        }
+        //     let mut i = (c - 1) as i32;
+        //     let mut j = c;
+        //     let mut found = true;
+        //     while i >= 0 && j < cols {
+        //         if columns[i as usize] != columns[j] {
+        //             found = false;
+        //             break;
+        //         }
+        //         i -= 1;
+        //         j += 1;
+        //     }
 
-        Pattern { score }
+        //     if found {
+        //         score += c;
+        //         break;
+        //     }
+        // }
+
+        Pattern { score1, score2 }
     }
 }
 
@@ -134,8 +195,8 @@ mod test {
             "..##..##.".to_string(),
             "#.#.##.#.".to_string(),
         ];
-        let pattern: Pattern = (&input).try_into().unwrap();
-        assert_eq!(pattern.score, 5);
+        let pattern: Pattern = (&input).into();
+        assert_eq!(pattern.score1, 5);
     }
 
     #[test]
@@ -149,7 +210,7 @@ mod test {
             "..##..###".to_string(),
             "#....#..#".to_string(),
         ];
-        let pattern: Pattern = (&input).try_into().unwrap();
-        assert_eq!(pattern.score, 400);
+        let pattern: Pattern = (&input).into();
+        assert_eq!(pattern.score1, 400);
     }
 }
